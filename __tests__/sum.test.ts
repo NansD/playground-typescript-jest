1b7437edd66b24c9efafcad593dddef0668095d9
Nans Dumortier <nans.dumortier-isc.france@ibm.com>
2020-03-18 21:53:41 +0100

    chore(lint): add tslint with config airbnb

--- __tests__/sum.test.ts
+++ __tests__/sum.test.ts
@@ -1,7 +1,7 @@
 import sum from '../src/sum';
 
-describe("Example function", () => {
-  it('should sum 2 + 2 an equals 4', () => {
+describe("Sum", () => {
+  it('should sum 2 + 2 and return 4', () => {
     expect(sum(2, 2)).toBe(4);
   })
 })
